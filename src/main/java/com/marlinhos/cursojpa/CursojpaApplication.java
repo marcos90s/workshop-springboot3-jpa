dc09cd21436c8ed94b1311e08cdf4cc87c94cbbf
Marcos Antônio <marcosabes@hotmail.com>
2023-06-20 21:21:48 -0300

    Project created

---
+++ src/main/java/com/marlinhos/cursojpa/CursojpaApplication.java
@@ -0,0 +1,13 @@
+package com.marlinhos.cursojpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursojpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursojpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marlinhos/cursojpa/CursojpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marlinhos.cursojpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursojpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
