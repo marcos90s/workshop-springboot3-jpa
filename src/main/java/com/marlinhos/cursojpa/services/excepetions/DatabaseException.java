4cf9170cebb81508595ec305a777e989466737c2
Marcos Antônio <marcosabes@hotmail.com>
2023-06-26 19:07:38 -0300

    Exception handling - delete

---
+++ src/main/java/com/marlinhos/cursojpa/services/excepetions/DatabaseException.java
@@ -0,0 +1,9 @@
+package com.marlinhos.cursojpa.services.excepetions;
+
+public class DatabaseException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+	
+	public DatabaseException(String msg) {
+		super(msg);
+	}
+}
