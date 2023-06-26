3635bf6a548faf5bf8ee6ecb71c4deb1ce45b9a0
Marcos Antônio <marcosabes@hotmail.com>
2023-06-26 18:48:28 -0300

    Excepetion handling - findById

--- src/main/java/com/marlinhos/cursojpa/services/UserService.java
+++ src/main/java/com/marlinhos/cursojpa/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.marlinhos.cursojpa.entities.User;
 import com.marlinhos.cursojpa.repositories.UserRepository;
+import com.marlinhos.cursojpa.services.excepetions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 
 	public User insert(User obj) {
---
+++ src/main/java/com/marlinhos/cursojpa/services/excepetions/ResourceNotFoundException.java
@@ -0,0 +1,11 @@
+package com.marlinhos.cursojpa.services.excepetions;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+
+	public ResourceNotFoundException(Object id) {
+		super("Resource not found. Id "+ id);
+	}
+	
+}
