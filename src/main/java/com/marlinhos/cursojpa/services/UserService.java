26a2b67beec2d053513c6eec811aa14c7600acd2
Marcos Antônio <marcosabes@hotmail.com>
2023-06-26 19:11:29 -0300

    Exception handling - update

--- src/main/java/com/marlinhos/cursojpa/services/UserService.java
+++ src/main/java/com/marlinhos/cursojpa/services/UserService.java
@@ -13,6 +13,8 @@ import com.marlinhos.cursojpa.repositories.UserRepository;
 import com.marlinhos.cursojpa.services.excepetions.DatabaseException;
 import com.marlinhos.cursojpa.services.excepetions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 
@@ -43,9 +45,13 @@ public class UserService {
 	}
 
 	public User update(Long id, User obj) {
+		try {
 		User entity = repository.getReferenceById(id);
 		updateData(entity, obj);
 		return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
